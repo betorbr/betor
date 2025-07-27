b267034164e2825c1cce6fa5a9dc273c4384cfe1
Douglas Paz <eu@douglaspaz.com>
2025-07-27 16:26:08 -0300

    chore: update ADDONS type

--- betor_scrapy/settings.py
+++ betor_scrapy/settings.py
@@ -1,9 +1,9 @@
-from typing import Dict
+from typing import Dict, Type, Union
 
 BOT_NAME = "betor_scrapy"
 SPIDER_MODULES = ["betor_scrapy.spiders"]
 NEWSPIDER_MODULE = "betor_scrapy.spiders"
-ADDONS: Dict[str, int] = {}
+ADDONS: Dict[Union[str, Type], int] = {}
 ROBOTSTXT_OBEY = True
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
 DOWNLOAD_DELAY = 1
