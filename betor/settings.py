edc13565e44daa2807370840f19765613f7ef43d
Douglas Paz <eu@douglaspaz.com>
2025-07-29 13:39:05 -0300

    feat: remove download slots

--- betor/settings.py
+++ betor/settings.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from urllib.parse import urlparse
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -8,13 +7,6 @@ class FlareSolverrSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_prefix="flaresolverr_")
 
     base_url: Optional[str] = None
-
-    @property
-    def domain(self) -> Optional[str]:
-        if not self.base_url:
-            return None
-        parsed = urlparse(self.base_url)
-        return parsed.hostname
 
 
 flaresolverr_settings = FlareSolverrSettings()
