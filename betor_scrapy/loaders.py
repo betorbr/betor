1e09da95d874f5b14f635393b7fd40e57c22e7f0
Douglas Paz <eu@douglaspaz.com>
2025-07-30 21:24:04 -0300

    feat: init ProviderLoader

---
+++ betor_scrapy/loaders.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+import scrapy.http
+import scrapy.loader
+from itemloaders.processors import TakeFirst
+
+from betor.providers.provider import Provider
+from betor_scrapy.items import ProviderItem
+
+from .processors import Title
+
+
+class ProviderLoader(scrapy.loader.ItemLoader):
+    default_item_class = ProviderItem
+    default_output_processor = TakeFirst()
+
+    title_out = Title()
+    translated_title_out = Title()
+    raw_title_out = Title()
+
+    def __init__(
+        self,
+        provider: Provider,
+        item=None,
+        selector=None,
+        response: Optional[scrapy.http.Response] = None,
+        parent=None,
+        **context,
+    ):
+        super().__init__(item, selector, response, parent, **context)
+        self.add_value("provider_slug", provider.slug)
+        if response:
+            self.add_value("provider_url", response.url)
