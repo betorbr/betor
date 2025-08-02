ca4511eb25da5eb4d52d44ac68e119ea6fcc8255
Douglas Paz <eu@douglaspaz.com>
2025-08-02 16:04:41 -0300

    feat: scrapy item to item

--- betor_scrapy/items.py
+++ betor_scrapy/items.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from betor.types import Item
+
 
 class ScrapyItem(scrapy.Item):
     provider_slug = scrapy.Field()
@@ -12,3 +14,26 @@ class ScrapyItem(scrapy.Item):
     languages = scrapy.Field()
     magnet_links = scrapy.Field()
     imdb_id = scrapy.Field()
+
+    def to_item(self) -> Item:
+        provider_slug = self.get("provider_slug")
+        provider_url = self.get("provider_url")
+        assert (
+            provider_slug and provider_url
+        ), f"Required {provider_slug=} {provider_url=}"
+        return {
+            "id": None,
+            "hash": None,
+            "inserted_at": None,
+            "updated_at": None,
+            "provider_slug": provider_slug,
+            "provider_url": provider_url,
+            "imdb_id": self.get("imdb_id"),
+            "magnet_links": self.get("magnet_links", []),
+            "languages": list(self.get("languages", set())),
+            "qualitys": list(self.get("qualitys", set())),
+            "title": self.get("title"),
+            "translated_title": self.get("translated_title"),
+            "raw_title": self.get("raw_title"),
+            "year": self.get("year"),
+        }
