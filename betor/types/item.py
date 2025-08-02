3eb1f86a4e91040f1f6316be9b020bb9dc006932
Douglas Paz <eu@douglaspaz.com>
2025-08-02 16:04:05 -0300

    feat: add item type

---
+++ betor/types/item.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from typing import List, Optional, TypedDict
+
+
+class Item(TypedDict):
+    id: Optional[str]
+    hash: Optional[int]
+    inserted_at: Optional[datetime]
+    updated_at: Optional[datetime]
+    provider_slug: str
+    provider_url: str
+    imdb_id: Optional[str]
+    magnet_links: List[str]
+    languages: List[str]
+    qualitys: List[str]
+    title: Optional[str]
+    translated_title: Optional[str]
+    raw_title: Optional[str]
+    year: Optional[int]
