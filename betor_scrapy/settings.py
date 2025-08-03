eb3430c4c22eb93035920b07c3aa750023fdc59b
Douglas Paz <eu@douglaspaz.com>
2025-08-03 12:33:28 -0300

    feat: enable httpcache

--- betor_scrapy/settings.py
+++ betor_scrapy/settings.py
@@ -7,6 +7,7 @@ ROBOTSTXT_OBEY = False
 DOWNLOAD_DELAY = 1
 FEED_EXPORT_ENCODING = "utf-8"
 DOWNLOADER_MIDDLEWARES = {
+    "scrapy.downloadermiddlewares.httpcache.HttpCacheMiddleware": 100,
     "betor_scrapy.middlewares.CloudflareDownloaderMiddleware": 551,
     "betor_scrapy.middlewares.CloudflareDownloaderResponseMiddleware": 552,
 }
@@ -15,3 +16,6 @@ FLARESOLVERR_BASE_URL = flaresolverr_settings.base_url
 ITEM_PIPELINES = {
     "betor_scrapy.pipelines.ItemsRepositoryPipeline": 300,
 }
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 14400
+HTTPCACHE_STORAGE = "betor_scrapy.httpcache.RedisCacheStorage"
