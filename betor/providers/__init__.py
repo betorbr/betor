3d4c9ad5b31fb169822d8dad1bb41ffe0962e69d
Douglas Paz <eu@douglaspaz.com>
2025-08-03 11:18:07 -0300

    feat: add PROVIDERS

--- betor/providers/__init__.py
+++ betor/providers/__init__.py
@@ -2,4 +2,9 @@ from .bludv import bludv
 from .comando_torrents import comando_torrents
 from .provider import Provider
 
+PROVIDERS = [
+    comando_torrents,
+    bludv,
+]
+
 __all__ = ["Provider", "bludv", "comando_torrents"]
