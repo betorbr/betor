e87d5152a35f95221eacd4fb440b362ce12cd1ef
Douglas Paz <eu@douglaspaz.com>
2025-08-03 11:20:46 -0300

    feat: init bludv spider

---
+++ betor_scrapy/spiders/bludv.py
@@ -0,0 +1,52 @@
+import scrapy
+import scrapy.http
+
+from betor.providers import bludv
+from betor_scrapy.loaders import ProviderLoader
+from betor_scrapy.utils import extract_fields
+
+
+class BludvSpider(scrapy.Spider):
+    name = bludv.slug
+    allowed_domains = bludv.domains
+    start_urls = [
+        bludv.get_page_url(),
+        bludv.get_page_url(1),
+        bludv.get_page_url(2),
+    ]
+
+    def parse(self, response: scrapy.http.Response):
+        if response.xpath(
+            "//div[@class='post']//div[@class='content']//p//a[@class='more-link']"
+        ):
+            yield from self.parse_page(response)
+        else:
+            yield from self.parse_item(response)
+
+    def parse_page(self, response: scrapy.http.Response):
+        for item_url in response.xpath(
+            "//div[@class='posts']//div[@class='post']//div[@class='title']//a/@href"
+        ).getall():
+            yield scrapy.http.Request(item_url)
+
+    def parse_item(self, response: scrapy.http.Response):
+        assert isinstance(response, scrapy.http.TextResponse)
+        loader = ProviderLoader(bludv, response=response)
+        informacoes_text = [
+            t.strip()
+            for t in response.xpath(
+                "(//div[@class='post']//div[@class='content']//p)[strong]//text()"
+            ).getall()
+            if t.strip() not in ["", "\n", ":", ">>INFORMAÇÕES<<"]
+        ]
+        for field, value in extract_fields(informacoes_text):
+            loader.add_value(field, value)
+        loader.add_xpath(
+            "raw_title", "//div[@class='post']//div[@class='title']//h1/text()"
+        )
+        loader.add_xpath("magnet_links", "//a[starts-with(@href, 'magnet')]/@href")
+        loader.add_xpath(
+            "imdb_id",
+            "//div[@class='post']//a[starts-with(@href, 'https://www.imdb.com')]/@href",
+        )
+        yield loader.load_item()
