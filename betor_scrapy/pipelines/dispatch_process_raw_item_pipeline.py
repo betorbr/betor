82ca2bf096cc484441d771101924f2d090aa9009
Douglas Paz <eu@douglaspaz.com>
2025-08-12 19:29:03 -0300

    feat: dispatch with signature

--- betor_scrapy/pipelines/dispatch_process_raw_item_pipeline.py
+++ betor_scrapy/pipelines/dispatch_process_raw_item_pipeline.py
@@ -1,8 +1,10 @@
-from betor.celery import process_raw_item
+from betor.celery import celery_app
 from betor_scrapy.items import ScrapyItem
 
 
 class DispatchProcessRawItemPipeline:
     async def process_item(self, item: ScrapyItem, spider):
-        process_raw_item.delay(item["provider_slug"], item["provider_url"])
+        celery_app.signature("process_raw_item").delay(
+            item["provider_slug"], item["provider_url"]
+        )
         return item
