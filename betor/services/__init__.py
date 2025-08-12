e38e5d720f43e6444077b89edf7747f0ab3f84b5
Douglas Paz <eu@douglaspaz.com>
2025-08-12 19:39:21 -0300

    feat: init update item torrent info service

--- betor/services/__init__.py
+++ betor/services/__init__.py
@@ -1,4 +1,9 @@
 from .determines_imdb_tmdb_ids_service import DeterminesIMDbTMDBIdsService
 from .process_raw_item_service import ProcessRawItemService
+from .update_item_torrent_info_service import UpdateItemTorrentInfoService
 
-__all__ = ["DeterminesIMDbTMDBIdsService", "ProcessRawItemService"]
+__all__ = [
+    "DeterminesIMDbTMDBIdsService",
+    "ProcessRawItemService",
+    "UpdateItemTorrentInfoService",
+]
---
+++ betor/services/update_item_torrent_info_service.py
@@ -0,0 +1,39 @@
+import tempfile
+
+import libtorrent as lt
+import motor.motor_asyncio
+
+from betor.repositories import ItemsRepository
+from betor.types import TorrentInfo
+
+
+class UpdateItemTorrentInfoService:
+    def __init__(self, mongodb_client: motor.motor_asyncio.AsyncIOMotorClient):
+        self.items_repository = ItemsRepository(mongodb_client)
+
+    async def update(self, magnet_uri: str):
+        torrent_info = self.get_info_from_lt_session(magnet_uri)
+        await self.items_repository.update_torrent_info(magnet_uri, torrent_info)
+        return torrent_info
+
+    def get_info_from_lt_session(self, magnet_uri: str) -> TorrentInfo:
+        with tempfile.TemporaryDirectory() as save_path:
+            lt_session = lt.session()
+            lt_add_torrent_params = lt.parse_magnet_uri(magnet_uri)
+            lt_add_torrent_params.save_path = save_path
+            lt_torrent_handler = lt_session.add_torrent(lt_add_torrent_params)
+            while True:
+                lt_torrent_status = lt_torrent_handler.status()
+                lt_torrent_info = lt_torrent_handler.torrent_file()
+                if lt_torrent_info:
+                    lt_torrent_handler.pause(lt_torrent_handler.graceful_pause)
+                    lt_file_storage = lt_torrent_info.orig_files()
+                    return TorrentInfo(
+                        torrent_name=lt_file_storage.name(),
+                        torrent_num_peers=lt_torrent_status.num_peers,
+                        torrent_num_seeds=lt_torrent_status.num_seeds,
+                        torrent_files=[
+                            lt_file_storage.file_name(i)
+                            for i in range(lt_file_storage.num_files())
+                        ],
+                    )
