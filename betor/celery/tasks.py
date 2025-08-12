cc2b70352a4ddab1b0a4234625f8e20141d20f63
Douglas Paz <eu@douglaspaz.com>
2025-08-12 19:43:31 -0300

    feat: add update item torrent info celery task

--- betor/celery/tasks.py
+++ betor/celery/tasks.py
@@ -3,7 +3,7 @@ import asyncio
 from celery import Task
 
 from betor.databases.mongodb import get_mongodb_client
-from betor.services import ProcessRawItemService
+from betor.services import ProcessRawItemService, UpdateItemTorrentInfoService
 
 from .app import celery_app
 
@@ -14,4 +14,13 @@ def _process_raw_item(provider_slug: str, provider_url: str):
     return asyncio.run(service.process(provider_slug, provider_url))
 
 
+def _update_item_torrent_info(magnet_uri: str):
+    mongodb_client = get_mongodb_client()
+    service = UpdateItemTorrentInfoService(mongodb_client)
+    return asyncio.run(service.update(magnet_uri))
+
+
 process_raw_item: Task = celery_app.task(_process_raw_item, name="process_raw_item")
+update_item_torrent_info: Task = celery_app.task(
+    _update_item_torrent_info, name="update_item_torrent_info"
+)
