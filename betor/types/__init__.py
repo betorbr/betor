f1451141c0a132243323b16830b2367a405b9e91
Douglas Paz <eu@douglaspaz.com>
2025-08-12 17:50:06 -0300

    feat: init torrent info

--- betor/types/__init__.py
+++ betor/types/__init__.py
@@ -1,5 +1,6 @@
 from .item import BaseItem, Item
 from .item_type import ItemType
 from .raw_item import RawItem
+from .torrent_info import TorrentInfo
 
-__all__ = ["RawItem", "BaseItem", "ItemType", "Item"]
+__all__ = ["RawItem", "BaseItem", "ItemType", "Item", "TorrentInfo"]
---
+++ betor/types/torrent_info.py
@@ -0,0 +1,8 @@
+from typing import List, Optional, TypedDict
+
+
+class TorrentInfo(TypedDict):
+    torrent_name: Optional[str]
+    torrent_num_peers: Optional[int]
+    torrent_num_seeds: Optional[int]
+    torrent_files: Optional[List[str]]
