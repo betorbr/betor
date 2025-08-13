44ceacb3b832cb7f63c04c07c859551ef1054784
Douglas Paz <eu@douglaspaz.com>
2025-08-13 13:42:40 -0300

    feat: enable spider search

--- betor_scrapy/spiders/provider_spider.py
+++ betor_scrapy/spiders/provider_spider.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import scrapy
 
 from betor.providers import Provider
@@ -5,12 +7,17 @@ from betor.providers import Provider
 
 class ProviderSpider:
     provider: Provider
+    deep: int
+    q: Optional[str]
 
-    def __init__(self, *args, deep: str | int = 1, **kwargs):
-        super().__init__(*args, deep=int(deep), **kwargs)  # type: ignore
+    def __init__(self, *args, deep: str | int = 1, q: Optional[str] = None, **kwargs):
+        super().__init__(*args, deep=int(deep), q=q, **kwargs)  # type: ignore
+
+    def page_url(self, page_n: int) -> str:
+        if self.q:
+            return self.provider.get_search_url(self.q, page_n)
+        return self.provider.get_page_url(page_n)
 
     def start_requests(self):
-        for url in [
-            self.provider.get_page_url(page_n) for page_n in range(1, self.deep + 1)
-        ]:
+        for url in [self.page_url(page_n) for page_n in range(1, self.deep + 1)]:
             yield scrapy.Request(url, dont_filter=True, flags=["no-cache"])
