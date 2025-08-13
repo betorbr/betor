09a26c63aed6713d59b8eb119e484b18934885a3
Douglas Paz <eu@douglaspaz.com>
2025-08-13 10:32:51 -0300

    feat: setup pagination

--- betor/api/app.py
+++ betor/api/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 
 from .v1.router import router as router_v1
 
 app = FastAPI()
+add_pagination(app)
 app.include_router(router_v1, prefix="/v1")
