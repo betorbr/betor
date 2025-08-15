637664dbdbbc7ad5c993a9e0d97ee7de871026a9
Douglas Paz <eu@douglaspaz.com>
2025-08-15 17:43:33 -0300

    feat: add redis client at fast api app

--- betor/api/app.py
+++ betor/api/app.py
@@ -1,6 +1,7 @@
 from fastapi_pagination import add_pagination
 
 from betor.databases.mongodb import get_mongodb_client
+from betor.databases.redis import get_redis_client
 
 from .fast_api import BetorFastAPI
 from .v1.router import router as router_v1
@@ -8,8 +9,10 @@ from .v1.router import router as router_v1
 
 async def lifespan(app: BetorFastAPI):
     app.mongodb_client = get_mongodb_client()
+    app.redis_client = get_redis_client()
     yield
     app.mongodb_client.close()
+    app.redis_client.close()
 
 
 app = BetorFastAPI(lifespan=lifespan)
--- betor/api/fast_api.py
+++ betor/api/fast_api.py
@@ -1,9 +1,11 @@
 import motor.motor_asyncio
+import redis
 from fastapi import FastAPI, Request
 
 
 class BetorFastAPI(FastAPI):
     mongodb_client: motor.motor_asyncio.AsyncIOMotorClient
+    redis_client: redis.Redis
 
 
 class BetorRequest(Request):
