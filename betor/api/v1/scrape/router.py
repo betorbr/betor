3f6e05b6f5f160b36e128009c37da6b761e316ad
Douglas Paz <eu@douglaspaz.com>
2025-08-15 18:52:38 -0300

    feat: add job monitor at scrape service

--- betor/api/v1/scrape/router.py
+++ betor/api/v1/scrape/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from betor.api.fast_api import BetorRequest
 from betor.services.scrape_service import ScrapeService
 
 from .schemas import ScrapePayload, ScrapeResponse
@@ -8,7 +9,7 @@ scrape_router = APIRouter()
 
 
 @scrape_router.post("/")
-async def scrape(payload: ScrapePayload) -> ScrapeResponse:
-    service = ScrapeService()
+async def scrape(request: BetorRequest, payload: ScrapePayload) -> ScrapeResponse:
+    service = ScrapeService(request.app.redis_client)
     scrape_return = await service.scrape(deep=payload.deep, q=payload.q)
     return ScrapeResponse(scrape_return=scrape_return)
--- betor/services/scrape_service.py
+++ betor/services/scrape_service.py
@@ -1,25 +1,51 @@
 import asyncio
 from typing import List, Optional, TypedDict
 
-from betor.providers import PROVIDERS
+import redis
+
+from betor.entities import Job, JobMonitor
+from betor.providers import PROVIDERS, Provider
+from betor.repositories import JobMonitorRepository
 from betor.use_services import ScrapydScheduleResponse, ScrapydUseService
 
 
 class ScrapeReturn(TypedDict):
     scrapyd_schedules_response: List[ScrapydScheduleResponse]
+    job_monitor: JobMonitor
 
 
 class ScrapeService:
-    def __init__(self):
+    def __init__(self, redis_client: redis.Redis):
         self.scrapyd_use_service = ScrapydUseService()
+        self.job_monitor_repository = JobMonitorRepository(redis_client)
 
     async def scrape(self, deep: int = 3, q: Optional[str] = None) -> ScrapeReturn:
+        job_monitor = self.job_monitor_repository.create()
         scrapyd_schedules_response = await asyncio.gather(
             *[
-                self.scrapyd_use_service.schedule(
-                    "betor", provider.slug, deep=deep, q=q
-                )
+                self.scrape_provider_and_add_job(provider, job_monitor, deep, q)
                 for provider in PROVIDERS
             ]
         )
-        return ScrapeReturn(scrapyd_schedules_response=scrapyd_schedules_response)
+        return ScrapeReturn(
+            scrapyd_schedules_response=scrapyd_schedules_response,
+            job_monitor=job_monitor,
+        )
+
+    async def scrape_provider_and_add_job(
+        self, provider: Provider, job_monitor: JobMonitor, deep: int, q: Optional[str]
+    ) -> ScrapydScheduleResponse:
+        scrapyd_schedule_response = await self.scrapyd_use_service.schedule(
+            "betor",
+            provider.slug,
+            deep=deep,
+            q=q,
+            job_monitor_id=job_monitor["id"],
+            job_index=provider.slug,
+        )
+        self.job_monitor_repository.add_job(
+            job_monitor,
+            Job(type="scrapd-schedule", id=scrapyd_schedule_response["jobid"]),
+            job_index=provider.slug,
+        )
+        return scrapyd_schedule_response
