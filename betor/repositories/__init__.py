f45a70bd39d20af8e9e7f53811fb52eb9f95904c
Douglas Paz <eu@douglaspaz.com>
2025-08-16 00:15:15 -0300

    feat: import JobMonitorNotFound from betor.repositories

--- betor/repositories/__init__.py
+++ betor/repositories/__init__.py
@@ -1,5 +1,10 @@
 from .items_repository import ItemsRepository
-from .job_monitor_repository import JobMonitorRepository
+from .job_monitor_repository import JobMonitorNotFound, JobMonitorRepository
 from .raw_items_repository import RawItemsRepository
 
-__all__ = ["RawItemsRepository", "ItemsRepository", "JobMonitorRepository"]
+__all__ = [
+    "RawItemsRepository",
+    "ItemsRepository",
+    "JobMonitorRepository",
+    "JobMonitorNotFound",
+]
