3ef9f30267368dbbbb25497502f1edffa1e12934
Douglas Paz <eu@douglaspaz.com>
2025-08-16 01:45:37 -0300

    feat: insert or update raw item add supports to job monitor

--- betor/services/insert_or_update_raw_item_service.py
+++ betor/services/insert_or_update_raw_item_service.py
@@ -1,16 +1,34 @@
+from typing import Optional
+
+import celery.result
 import motor.motor_asyncio
+import redis
 
 from betor.celery.app import celery_app
-from betor.entities import RawItem
-from betor.repositories import RawItemsRepository
+from betor.entities import Job, RawItem
+from betor.repositories import JobMonitorRepository, RawItemsRepository
 
 
 class InsertOrUpdateRawItemService:
-    def __init__(self, mongodb_client: motor.motor_asyncio.AsyncIOMotorClient):
+    def __init__(
+        self,
+        mongodb_client: motor.motor_asyncio.AsyncIOMotorClient,
+        redis_client: redis.Redis,
+    ):
         self.raw_items_repository = RawItemsRepository(mongodb_client)
+        self.job_monitor_repository = JobMonitorRepository(redis_client)
 
-    async def insert_or_update(self, raw_item: RawItem):
+    async def insert_or_update(
+        self,
+        raw_item: RawItem,
+        job_monitor_id: Optional[str] = None,
+        job_index: Optional[str] = None,
+    ):
         await self.raw_items_repository.insert_or_update(raw_item)
-        celery_app.signature("process_raw_item").delay(
-            raw_item["provider_slug"], raw_item["provider_url"]
-        )
+        result: celery.result.AsyncResult = celery_app.signature(
+            "process_raw_item"
+        ).delay(raw_item["provider_slug"], raw_item["provider_url"])
+        if job_monitor_id and job_index:
+            self.job_monitor_repository.add_job(
+                job_monitor_id, Job(type="celery-task", id=result.id)
+            )
--- betor_scrapy/pipelines/raw_items_pipeline.py
+++ betor_scrapy/pipelines/raw_items_pipeline.py
@@ -1,4 +1,9 @@
+from typing import Optional
+
+import scrapy
+
 from betor.databases.mongodb import get_mongodb_client
+from betor.databases.redis import get_redis_client
 from betor.services import InsertOrUpdateRawItemService
 from betor_scrapy.items import ScrapyItem
 
@@ -6,14 +11,20 @@ from betor_scrapy.items import ScrapyItem
 class RawItemsPipeline:
     def open_spider(self, spider):
         self.mongodb_client = get_mongodb_client()
+        self.redis_client = get_redis_client()
         self.insert_or_update_raw_item_service = InsertOrUpdateRawItemService(
-            self.mongodb_client
+            self.mongodb_client, self.redis_client
         )
 
     def close_spider(self, spider):
         self.mongodb_client.close()
+        self.redis_client.close()
 
-    async def process_item(self, item: ScrapyItem, spider):
+    async def process_item(self, item: ScrapyItem, spider: scrapy.Spider):
+        job_monitor_id: Optional[str] = getattr(spider, "job_monitor_id", None)
+        job_index: Optional[str] = getattr(spider, "job_index", None)
         raw_item = item.to_raw_item()
-        await self.insert_or_update_raw_item_service.insert_or_update(raw_item)
+        await self.insert_or_update_raw_item_service.insert_or_update(
+            raw_item, job_monitor_id=job_monitor_id, job_index=job_index
+        )
         return item
