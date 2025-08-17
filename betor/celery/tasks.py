eecc15f958b39e2afbbdf8f7ea4b90dbe4545ab9
Douglas Paz <eu@douglaspaz.com>
2025-08-16 23:42:29 -0300

    feat: add betor celery task with after return add job result

--- betor/celery/tasks.py
+++ betor/celery/tasks.py
@@ -3,18 +3,36 @@ import asyncio
 from celery import Task
 
 from betor.databases.mongodb import get_mongodb_client
-from betor.services import ProcessRawItemService, UpdateItemTorrentInfoService
+from betor.databases.redis import get_redis_client
+from betor.services import (
+    AddJobResultsService,
+    ProcessRawItemService,
+    UpdateItemTorrentInfoService,
+)
 
 from .app import celery_app
 
 
-def _process_raw_item(provider_slug: str, provider_url: str):
+class BetorCeleryTask(Task):
+    abstract = True
+
+    def after_return(self, status, retval, task_id, args, kwargs: dict, einfo):
+        job_monitor_id = kwargs.get("job_monitor_id")
+        job_index = kwargs.get("job_index")
+        if job_monitor_id and job_index:
+            redis_client = get_redis_client()
+            add_job_results_service = AddJobResultsService(redis_client)
+            add_job_results_service.add(job_monitor_id, job_index, retval)
+            redis_client.close()
+
+
+def _process_raw_item(provider_slug: str, provider_url: str, **kwargs):
     mongodb_client = get_mongodb_client()
     service = ProcessRawItemService(mongodb_client)
     return asyncio.run(service.process(provider_slug, provider_url))
 
 
-def _update_item_torrent_info(magnet_uri: str):
+def _update_item_torrent_info(magnet_uri: str, **kwargs):
     mongodb_client = get_mongodb_client()
     service = UpdateItemTorrentInfoService(mongodb_client)
     return asyncio.run(service.update(magnet_uri))
@@ -22,6 +40,7 @@ def _update_item_torrent_info(magnet_uri: str):
 
 process_raw_item: Task = celery_app.task(
     _process_raw_item,
+    base=BetorCeleryTask,
     name="process_raw_item",
     priority=9,
     default_retry_delay=15,
@@ -30,6 +49,7 @@ process_raw_item: Task = celery_app.task(
 )
 update_item_torrent_info: Task = celery_app.task(
     _update_item_torrent_info,
+    base=BetorCeleryTask,
     name="update_item_torrent_info",
     priority=0,
     soft_time_limit=(5 * 60),
