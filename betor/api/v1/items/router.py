4073639cceb02b6d605591e27072ad137648f48b
Douglas Paz <eu@douglaspaz.com>
2025-08-17 19:08:09 -0300

    feat: add items params

--- betor/api/v1/items/router.py
+++ betor/api/v1/items/router.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from fastapi import APIRouter
 from fastapi_pagination import Page
 from fastapi_pagination.ext.motor import apaginate
 
 from betor.api.fast_api import BetorRequest
-from betor.enums import ItemsSortEnum
+from betor.enums import ItemsSortEnum, ItemType
 from betor.services import ListItemsService
 
 from .schemas import ItemSchema
@@ -13,12 +15,19 @@ items_router = APIRouter()
 
 @items_router.get("/")
 async def list_items(
-    request: BetorRequest, sort: ItemsSortEnum = ItemsSortEnum.inserted_at_desc
+    request: BetorRequest,
+    sort: ItemsSortEnum = ItemsSortEnum.inserted_at_desc,
+    imdb_id: Optional[str] = None,
+    tmdb_id: Optional[str] = None,
+    item_type: Optional[ItemType] = None,
 ) -> Page[ItemSchema]:
     service = ListItemsService(request.app.mongodb_client)
-    collection, cursor_sort, transformer = service.apaginate_params(sort)
+    collection, query_filter, cursor_sort, transformer = service.apaginate_params(
+        sort, imdb_id=imdb_id, tmdb_id=tmdb_id, item_type=item_type
+    )
     return await apaginate(
         collection,
+        query_filter=query_filter,
         sort=cursor_sort,
         transformer=transformer,
     )
--- betor/services/list_items_service.py
+++ betor/services/list_items_service.py
@@ -1,9 +1,9 @@
-from typing import Callable, Dict, Sequence, Tuple, TypeAlias, Union
+from typing import Callable, Dict, Optional, Sequence, Tuple, TypeAlias, Union
 
 import motor.motor_asyncio
 
 from betor.entities import Item
-from betor.enums import ItemsSortEnum
+from betor.enums import ItemsSortEnum, ItemType
 from betor.repositories import ItemsRepository
 
 CursorSort: TypeAlias = Union[str, Tuple[str, int]]
@@ -26,15 +26,30 @@ class ListItemsService:
     def __init__(self, mongodb_client: motor.motor_asyncio.AsyncIOMotorClient):
         self.items_repository = ItemsRepository(mongodb_client)
 
-    def apaginate_params(self, sort: ItemsSortEnum) -> Tuple[
+    def apaginate_params(
+        self,
+        sort: ItemsSortEnum,
+        imdb_id: Optional[str],
+        tmdb_id: Optional[str],
+        item_type: Optional[ItemType],
+    ) -> Tuple[
         motor.motor_asyncio.AsyncIOMotorCollection,
+        Optional[Dict],
         CursorSort,
         Callable[[Sequence[Dict]], Sequence[Item]],
     ]:
         cursor_sort = CURSOR_SORT_MAPPING.get(sort)
         assert cursor_sort
+        and_statements = []
+        if imdb_id:
+            and_statements.append({"imdb_id": imdb_id})
+        if tmdb_id:
+            and_statements.append({"tmdb_id": tmdb_id})
+        if item_type:
+            and_statements.append({"item_type": item_type})
         return (
             self.items_repository.collection,
+            {"$and": and_statements} if and_statements else None,
             cursor_sort,
             ItemsRepository.parse_results,
         )
