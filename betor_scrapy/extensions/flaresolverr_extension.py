175a63caed3185197dda9a258b19c47549c0dbc5
Douglas Paz <eu@douglaspaz.com>
2025-08-16 21:14:12 -0300

    feat: improve flaresolverr extension session manager with redis

--- betor_scrapy/extensions/flaresolverr_extension.py
+++ betor_scrapy/extensions/flaresolverr_extension.py
@@ -1,68 +1,98 @@
-from typing import Self, Set, TypedDict, cast
+from typing import List, Self, Set, TypedDict, cast
+from uuid import uuid4
 
 import httpx
+import redis
 import scrapy.crawler
 import scrapy.exceptions
 import scrapy.signals
+
+from betor.databases.redis import get_redis_client
 
 
 class FlareSolverrSessionsCreateResponse(TypedDict):
     session: str
 
 
+class FlareSolverrListSessionsResponse(TypedDict):
+    sessions: List[str]
+
+
 class FlareSolverrExtension:
-    free_sessions: Set[str]
-    locked_sessions: Set[str]
+    @classmethod
+    def from_crawler(cls, crawler: scrapy.crawler.Crawler) -> Self:
+        base_url = crawler.settings.get("FLARESOLVERR_BASE_URL")
+        if not base_url:
+            raise scrapy.exceptions.NotConfigured
+        redis_client = get_redis_client()
+        session_prefix = crawler.settings.get(
+            "FLARESOLVERR_SESSION_PREFIX", "flaresolverr-extension:"
+        )
+        redis_locked_sessions_key = crawler.settings.get(
+            "FLARESOLVERR_REDIS_LOCKED_SESSIONS_KEY", "flaresolverr:locked_sessions"
+        )
+        obj = cls(redis_client, base_url, session_prefix, redis_locked_sessions_key)
+        crawler.signals.connect(obj.spider_opened, signal=scrapy.signals.spider_opened)
+        return obj
 
     def __init__(
         self,
-        flaresolverr_base_url: str,
-        free_sessions: Set[str],
-        locked_sessions: Set[str],
+        redis_client: redis.Redis,
+        base_url: str,
+        session_prefix: str,
+        redis_locked_sessions_key: str,
     ):
-        self.flaresolverr_base_url = flaresolverr_base_url
-        self.free_sessions = free_sessions
-        self.locked_sessions = locked_sessions
-
-    @classmethod
-    def from_crawler(cls, crawler: scrapy.crawler.Crawler) -> Self:
-        flaresolverr_base_url = crawler.settings.get("FLARESOLVERR_BASE_URL")
-        if not flaresolverr_base_url:
-            raise scrapy.exceptions.NotConfigured
-        obj = cls(flaresolverr_base_url, set(), set())
-        crawler.signals.connect(obj.spider_opened, signal=scrapy.signals.spider_opened)
-        crawler.signals.connect(obj.spider_closed, signal=scrapy.signals.spider_closed)
-        return obj
+        self.redis_client = redis_client
+        self.base_url = base_url
+        self.session_prefix = session_prefix
+        self.redis_locked_sessions_key = redis_locked_sessions_key
 
     def spider_opened(self, spider: scrapy.Spider) -> None:
+        self.redis_client.ping()
         spider.flaresolverr = self  # type: ignore[attr-defined]
 
-    def spider_closed(self, spider: scrapy.Spider) -> None:
-        sessions = {*self.free_sessions, *self.locked_sessions}
-        for session in sessions:
-            response = httpx.post(
-                f"{self.flaresolverr_base_url}/v1",
-                json={"cmd": "sessions.destroy", "session": session},
-            )
-            response.raise_for_status()
+    def get_available_sessions(self) -> Set[str]:
+        response = httpx.post(
+            f"{self.base_url}/v1",
+            json={"cmd": "sessions.list"},
+        )
+        response.raise_for_status()
+        data = cast(FlareSolverrListSessionsResponse, response.json())
+        return set(
+            [
+                session
+                for session in data["sessions"]
+                if session.startswith(self.session_prefix)
+            ]
+        )
+
+    def get_free_sessions(self) -> Set[str]:
+        available_sessions = self.get_available_sessions()
+        locked_sessions = cast(
+            Set, self.redis_client.smembers(self.redis_locked_sessions_key)
+        )
+        return set(available_sessions - locked_sessions)
 
     def get_free_session(self) -> str:
-        if not self.free_sessions:
-            self.create_session()
-            return self.get_free_session()
-        session = self.free_sessions.pop()
-        self.locked_sessions.add(session)
+        free_sessions = self.get_free_sessions()
+        try:
+            session = free_sessions.pop()
+        except KeyError:
+            session = self.create_session()
+        self.redis_client.sadd(self.redis_locked_sessions_key, session)
         return session
 
-    def create_session(self):
+    def create_session(self) -> str:
         response = httpx.post(
-            f"{self.flaresolverr_base_url}/v1", json={"cmd": "sessions.create"}
+            f"{self.base_url}/v1",
+            json={
+                "cmd": "sessions.create",
+                "session": f"{self.session_prefix}{uuid4()}",
+            },
         )
         response.raise_for_status()
         data = cast(FlareSolverrSessionsCreateResponse, response.json())
-        self.free_sessions.add(data["session"])
+        return data["session"]
 
     def free_session(self, session: str):
-        if session in self.locked_sessions:
-            self.locked_sessions.remove(session)
-        self.free_sessions.add(session)
+        self.redis_client.srem(self.redis_locked_sessions_key, session)
