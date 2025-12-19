c894d44e6cf6f2d6e54d849677fb3d173eb41632
Douglas Paz <eu@douglaspaz.com>
2025-12-19 10:57:52 -0300

    feat: improve stack filmes

--- betor/providers/starck_filmes.py
+++ betor/providers/starck_filmes.py
@@ -3,8 +3,8 @@ from .provider import Provider
 starck_filmes = Provider(
     "starck-filmes",
     "https://www.starckfilmes-v6.com",
-    "https://www.starckfilmes-v6.com/page/{page}/",
-    "https://www.starckfilmes-v6.com/?s={qs}",
-    "https://www.starckfilmes-v6.com/page/{page}/?s={qs}",
+    "{base_url}/page/{page}/",
+    "{base_url}/?s={qs}",
+    "{base_url}/page/{page}/?s={qs}",
     append_domains=["starckfilmes.com", "starckfilmes-v2.com", "starckfilmes-v7.com"],
 )
