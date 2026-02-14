0d87aeb7b423911c2aa455aefd59de63aca17db9
Douglas Paz <eu@douglaspaz.com>
2026-02-14 14:14:28 -0300

    feat: add ProviderURLIMDBMappingRepository to betor.repositories

--- betor/repositories/__init__.py
+++ betor/repositories/__init__.py
@@ -1,6 +1,7 @@
 from .catalog_items_repository import CatalogItemsRepository
 from .items_repository import ItemsRepository
 from .job_monitor_repository import JobMonitorRepository
+from .provider_url_imdb_mapping_repository import ProviderURLIMDBMappingRepository
 from .raw_items_repository import RawItemsRepository
 from .search_job_monitor_repository import SearchJobMonitorRepository
 
@@ -10,4 +11,5 @@ __all__ = [
     "JobMonitorRepository",
     "SearchJobMonitorRepository",
     "CatalogItemsRepository",
+    "ProviderURLIMDBMappingRepository",
 ]
