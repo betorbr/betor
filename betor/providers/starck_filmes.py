4d28bcbd84be9a54b124d54dbdb2024c1bb355bf
Douglas Paz <eu@douglaspaz.com>
2026-02-15 12:51:28 -0300

    fix: stack filmes domain

--- betor/providers/starck_filmes.py
+++ betor/providers/starck_filmes.py
@@ -2,7 +2,7 @@ from .provider import Provider
 
 starck_filmes = Provider(
     "starck-filmes",
-    "https://www.starckfilmes-v8.com",
+    "https://www.starckfilmes-v10.com",
     "{base_url}/page/{page}/",
     "{base_url}/?s={qs}",
     "{base_url}/page/{page}/?s={qs}",
@@ -11,5 +11,6 @@ starck_filmes = Provider(
         "starckfilmes-v2.com",
         "starckfilmes-v7.com",
         "starckfilmes-v6.com",
+        "starckfilmes-v8.com",
     ],
 )
