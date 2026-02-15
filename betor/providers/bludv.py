dd7dd09630a36b12eeea0c6f45c9bc682a01358b
Douglas Paz <eu@douglaspaz.com>
2026-02-15 12:48:59 -0300

    fix: bludv domain

--- betor/providers/bludv.py
+++ betor/providers/bludv.py
@@ -2,10 +2,10 @@ from .provider import Provider
 
 bludv = Provider(
     "bludv",
-    "https://bludv-v1.xyz",
+    "https://bludv1.xyz",
     "{base_url}/page/{page}/",
     "{base_url}/?s={qs}",
     "{base_url}/page/{page}/?s={qs}",
-    append_domains=["bludv.net"],
-    cf_clearance_domain=".bludv-v1.xyz",
+    append_domains=["bludv.net", "bludv-v1.xyz"],
+    cf_clearance_domain=".bludv1.xyz",
 )
