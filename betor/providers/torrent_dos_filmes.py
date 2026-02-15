7b8f9a72fde00ada47a70b2e5f37a59bc99453bb
Douglas Paz <eu@douglaspaz.com>
2026-02-15 12:53:47 -0300

    fix: torrent dos filmes domain

--- betor/providers/torrent_dos_filmes.py
+++ betor/providers/torrent_dos_filmes.py
@@ -2,8 +2,9 @@ from .provider import Provider
 
 torrent_dos_filmes = Provider(
     "torrent-dos-filmes",
-    "https://torrentdosfilmes.se/?s",
-    "https://torrentdosfilmes.se/page/{page}/?s",
-    "https://torrentdosfilmes.se/?s={qs}",
-    "https://torrentdosfilmes.se/page/{page}/?s={qs}",
+    "https://torrentdosfilmes-v2.xyz/?s",
+    "https://torrentdosfilmes-v2.xyz/page/{page}/?s",
+    "https://torrentdosfilmes-v2.xyz/?s={qs}",
+    "https://torrentdosfilmes-v2.xyz/page/{page}/?s={qs}",
+    append_domains=["torrentdosfilmes.se"],
 )
