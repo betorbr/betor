2c208bec51500380df66ded5f7f43a5641c4072a
Douglas Paz <eu@douglaspaz.com>
2026-05-17 16:00:33 -0300

    feat: update bludv base url

--- betor/providers/bludv.py
+++ betor/providers/bludv.py
@@ -2,10 +2,10 @@ from .provider import Provider
 
 bludv = Provider(
     "bludv",
-    "https://bludv1.com",
+    "https://bludv2.xyz",
     "{base_url}/page/{page}/",
     "{base_url}/?s={qs}",
     "{base_url}/page/{page}/?s={qs}",
-    append_domains=["bludv.net", "bludv-v1.xyz", "bludv1.xyz"],
-    cf_clearance_domain=".bludv1.com",
+    append_domains=["bludv.net", "bludv-v1.xyz", "bludv1.xyz", "bludv1.com"],
+    cf_clearance_domain=".bludv2.xyz",
 )
