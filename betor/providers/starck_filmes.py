a900c91738f8acddb4f928391b2c9dc1ca819975
Douglas Paz <eu@douglaspaz.com>
2026-06-02 22:23:13 -0300

    feat: update starck filmes url

--- betor/providers/starck_filmes.py
+++ betor/providers/starck_filmes.py
@@ -2,7 +2,7 @@ from .provider import Provider
 
 starck_filmes = Provider(
     "starck-filmes",
-    "https://www.starckfilmes-v16.com",
+    "https://www.starckfilmes-v11.com",
     "{base_url}/page/{page}/",
     "{base_url}/?s={qs}",
     "{base_url}/page/{page}/?s={qs}",
@@ -16,5 +16,6 @@ starck_filmes = Provider(
         "starckfilmes-v12.com",
         "starckfilmes-v14.com",
         "starckfilmes-v15.com",
+        "starckfilmes-v16.com",
     ],
 )
