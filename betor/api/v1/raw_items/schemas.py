05fa25d1617f2ecf9cf3eb42d3688aacd6599e35
Douglas Paz <eu@douglaspaz.com>
2026-06-28 11:04:46 -0300

    feat: init raw items api v1

---
+++ betor/api/v1/raw_items/schemas.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class RawItemSchema(BaseModel):
+    id: Optional[str]
+    hash: Optional[int]
+    inserted_at: Optional[datetime]
+    updated_at: Optional[datetime]
+    provider_slug: str
+    provider_url: str
+    imdb_id: Optional[str]
+    tmdb_id: Optional[str]
+    magnet_uris: List[str]
+    languages: List[str]
+    qualitys: List[str]
+    title: Optional[str]
+    translated_title: Optional[str]
+    raw_title: Optional[str]
+    year: Optional[int]
+    cast: Optional[List[str]]
