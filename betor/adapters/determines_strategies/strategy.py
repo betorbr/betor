ce6aca953439cad76aa7dd2bbdd9e7da8dddb2a8
Douglas Paz <eu@douglaspaz.com>
2026-07-12 20:43:04 -0300

    chore: move build_queries to tmdb treding strategy

--- betor/adapters/determines_strategies/strategy.py
+++ betor/adapters/determines_strategies/strategy.py
@@ -1,24 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Generator, Optional
+from typing import Optional
 
 from betor.entities import RawItem
 from betor.types import Scores, StrategyGenerator
 
 
 class Strategy(ABC):
-    @staticmethod
-    def build_queries(raw_item: RawItem) -> Generator[str, None, None]:
-        if raw_item["title"] and raw_item["year"]:
-            yield f"{raw_item['title']} {raw_item['year']}"
-        if raw_item["translated_title"] and raw_item["year"]:
-            yield f"{raw_item['translated_title']} {raw_item['year']}"
-        if raw_item["title"]:
-            yield raw_item["title"]
-            if "/" in raw_item["title"]:
-                yield from map(lambda v: v.strip(), raw_item["title"].split("/"))
-        if raw_item["translated_title"]:
-            yield raw_item["translated_title"]
-
     @abstractmethod
     async def __call__(
         self,
