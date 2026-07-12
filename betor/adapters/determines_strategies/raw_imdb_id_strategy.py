0ab23486cb688934161549eb237087525f45daec
Douglas Paz <eu@douglaspaz.com>
2026-07-12 20:49:52 -0300

    feat: raw imdb id not use imdb title api

--- betor/adapters/determines_strategies/raw_imdb_id_strategy.py
+++ betor/adapters/determines_strategies/raw_imdb_id_strategy.py
@@ -1,17 +1,12 @@
 from typing import Optional
 
 from betor.entities import RawItem
-from betor.enums import ItemType
-from betor.external_apis import IMDBAPIDevTitleAPI, IMDBAPIDevTitleAPIError
 from betor.types import Scores, StrategyGenerator
 
 from .strategy import Strategy
 
 
 class RawImdbIdStrategy(Strategy):
-    def __init__(self, imdb_title_api: IMDBAPIDevTitleAPI):
-        self.imdb_title_api = imdb_title_api
-
     async def __call__(
         self,
         raw_item: RawItem,
@@ -19,12 +14,4 @@ class RawImdbIdStrategy(Strategy):
         tmdb_scores: Optional[Scores] = None,
     ) -> StrategyGenerator:
         if raw_item["imdb_id"]:
-            try:
-                title = await self.imdb_title_api.execute(raw_item["imdb_id"])
-                if title["type"] == "movie":
-                    yield self, 100.0, title["id"], None, ItemType.movie
-                if title["type"] in ["tvSeries", "tvMiniSeries"]:
-                    yield self, 100.0, title["id"], None, ItemType.tv
-                return
-            except IMDBAPIDevTitleAPIError:
-                return
+            yield self, 100.0, raw_item["imdb_id"], None, None
